79123070fe3e548fde86542994e9b2521d76f5f7
Nomoto Kazuhiro <nomoto.kazuhiro@fujitsu.com>
2020-12-08 16:32:13 +0900

    Revert "Replaced cpu_barrier.(c|h)pp"
    This reverts commit 312c71b01f904429053ceadf2f462c2f575a989b.

--- src/cpu/aarch64/cpu_barrier.cpp
+++ src/cpu/aarch64/cpu_barrier.cpp
@@ -1,6 +1,5 @@
 /*******************************************************************************
-* Copyright 2020 Intel Corporation
-* Copyright 2020 FUJITSU LIMITED
+* Copyright 2017-2020 Intel Corporation
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -26,91 +25,76 @@ namespace aarch64 {
 
 namespace simple_barrier {
 
-void generate(jit_generator &code, Xbyak_aarch64::XReg reg_ctx,
-        Xbyak_aarch64::XReg reg_nthr) {
+void generate(
+        jit_generator &code, Xbyak::Reg64 reg_ctx, Xbyak::Reg64 reg_nthr) {
 #define BAR_CTR_OFF offsetof(ctx_t, ctr)
 #define BAR_SENSE_OFF offsetof(ctx_t, sense)
-    using namespace Xbyak_aarch64;
+    using namespace Xbyak;
 
-    const XReg x_tmp_0 = code.X_TMP_0;
-    const WReg w_tmp_1 = code.W_TMP_1;
-    const XReg x_addr_sense = code.X_TMP_2;
-    const XReg x_addr_ctx = code.X_TMP_3;
-    const XReg x_sense = code.X_TMP_4;
-    const XReg x_tmp_addr = code.X_DEFAULT_ADDR;
+    Xbyak::Reg64 reg_tmp = [&]() {
+        /* returns register which is neither reg_ctx nor reg_nthr */
+        Xbyak::Reg64 regs[] = {util::rax, util::rbx, util::rcx};
+        for (size_t i = 0; i < sizeof(regs) / sizeof(regs[0]); ++i)
+            if (!utils::one_of(regs[i], reg_ctx, reg_nthr)) return regs[i];
+        return regs[0]; /* should not happen */
+    }();
 
-    Label barrier_exit_label, spin_label, atomic_label;
+    Label barrier_exit_label, barrier_exit_restore_label, spin_label;
 
     code.cmp(reg_nthr, 1);
-    code.b(EQ, barrier_exit_label);
+    code.jbe(barrier_exit_label);
+
+    code.push(reg_tmp);
 
     /* take and save current sense */
-    code.add_imm(x_addr_sense, reg_ctx, BAR_SENSE_OFF, x_tmp_0);
-    code.ldr(x_sense, ptr(x_addr_sense));
+    code.mov(reg_tmp, code.ptr[reg_ctx + BAR_SENSE_OFF]);
+    code.push(reg_tmp);
+    code.mov(reg_tmp, 1);
 
-    code.add_imm(x_addr_ctx, reg_ctx, BAR_CTR_OFF, x_tmp_addr);
-    if (mayiuse(sve_512)) {
-        code.prfm(PLDL1KEEP, ptr(x_addr_ctx));
-        code.prfm(PLDL1KEEP, ptr(x_addr_ctx));
+    if (mayiuse(avx512_mic)) {
+        code.prefetchwt1(code.ptr[reg_ctx + BAR_CTR_OFF]);
+        code.prefetchwt1(code.ptr[reg_ctx + BAR_CTR_OFF]);
     }
 
-    if (mayiuse_atomic()) {
-        code.mov(x_tmp_0, 1);
-        code.ldaddal(x_tmp_0, x_tmp_0, ptr(x_addr_ctx));
-        code.add(x_tmp_0, x_tmp_0, 1);
-    } else {
-        code.L(atomic_label);
-        code.ldaxr(x_tmp_0, ptr(x_addr_ctx));
-        code.add(x_tmp_0, x_tmp_0, 1);
-        code.stlxr(w_tmp_1, x_tmp_0, ptr(x_addr_ctx));
-        code.cbnz(w_tmp_1, atomic_label);
-    }
-    code.cmp(x_tmp_0, reg_nthr);
-    code.b(NE, spin_label);
+    code.lock();
+    code.xadd(code.ptr[reg_ctx + BAR_CTR_OFF], reg_tmp);
+    code.add(reg_tmp, 1);
+    code.cmp(reg_tmp, reg_nthr);
+    code.pop(reg_tmp); /* restore previous sense */
+    code.jne(spin_label);
 
     /* the last thread {{{ */
-    code.mov_imm(x_tmp_0, 0);
-    code.str(x_tmp_0, ptr(x_addr_ctx)); // reset ctx
-    /* commit CTX clear, before modify SENSE,
-       otherwise other threads load old SENSE value. */
-    code.dmb(ISH);
+    code.mov(code.qword[reg_ctx + BAR_CTR_OFF], 0); // reset ctx
+#ifdef DNNL_INDIRECT_JIT_AARCH64
+    code.CodeGeneratorAArch64::dmb(Xbyak_aarch64::ISH);
+#endif //#ifdef DNNL_INDIRECT_JIT_AARCH64
 
     // notify waiting threads
-    code.mvn(x_sense, x_sense);
-    code.str(x_sense, ptr(x_addr_sense));
-    code.b(barrier_exit_label);
+    code.not_(reg_tmp);
+    code.mov(code.ptr[reg_ctx + BAR_SENSE_OFF], reg_tmp);
+    code.jmp(barrier_exit_restore_label);
     /* }}} the last thread */
 
-    code.L(spin_label);
-    code.yield();
-    code.ldr(x_tmp_0, ptr(x_addr_sense));
-    code.cmp(x_tmp_0, x_sense);
-    code.b(EQ, spin_label);
+    code.CodeGenerator::L(spin_label);
+    code.pause();
+    code.cmp(reg_tmp, code.ptr[reg_ctx + BAR_SENSE_OFF]);
+    code.je(spin_label);
 
-    code.dmb(ISH);
-    code.L(barrier_exit_label);
+#ifdef DNNL_INDIRECT_JIT_AARCH64
+    code.CodeGeneratorAArch64::dmb(Xbyak_aarch64::ISH);
+#endif //#ifdef DNNL_INDIRECT_JIT_AARCH64
 
+    code.CodeGenerator::L(barrier_exit_restore_label);
+    code.pop(reg_tmp);
+
+    code.CodeGenerator::L(barrier_exit_label);
 #undef BAR_CTR_OFF
 #undef BAR_SENSE_OFF
 }
 
-/** jit barrier generator */
-struct jit_t : public jit_generator {
-
-    void generate() override {
-        simple_barrier::generate(*this, abi_param1, abi_param2);
-        ret();
-    }
-
-    // TODO: Need to check status
-    jit_t() { create_kernel(); }
-
-    DECLARE_CPU_JIT_AUX_FUNCTIONS(jit_t)
-};
-
 void barrier(ctx_t *ctx, int nthr) {
     static jit_t j; /* XXX: constructed on load ... */
-    j(ctx, nthr);
+    j.barrier(ctx, nthr); // barrier
 }
 
 } // namespace simple_barrier
